ed751e469cec125a7c6e4cfe25e923a0ed13d9bb
Dusan Ilic <dule979@gmail.com>
2022-10-20 23:56:28 +0200

    onSubmit and onChange added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 import Header from './components/Header';
 import Search from './components/Search';
 
 function App() {
+  const [input, setInput] = useState('');
+
+  const handleInput = (e) => {
+    e.preventDefault();
+    console.log(input);
+  }
+
   return (
     <div>
       <Header title="Image Gallery" />
-      <Search />
+      <Search input={input} onInput={setInput} onHandleInput={handleInput} />
     </div>
   );
 }
--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-const Search = () => {
+const Search = ({ input, onInput, onHandleInput }) => {
   return (
-    <Container className="mt-4" >
+    <Container className="mt-4">
       <Row className="justify-content-center">
         <Col xs={12} md={8} lg={6}>
-          <Form>
+          <Form onSubmit={onHandleInput}>
             <Row>
               <Col xs={9}>
-                <Form.Control placeholder="Search for new images..." />
+                <Form.Control
+                  placeholder="Search for new images..."
+                  type="text"
+                  value={input}
+                  onChange={(e) => onInput(e.target.value)}
+                />
               </Col>
               <Col>
-                <Button variant="primary" type="submit">Search</Button>
+                <Button variant="primary" type="submit">
+                  Search
+                </Button>
               </Col>
             </Row>
           </Form>
