7cb4b9dc814686d4a3013f55df94c984fe731cba
Dusan Ilic <dule979@gmail.com>
2022-10-21 18:56:04 +0200

    First API connected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,30 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
-import Header from './components/Header';
-import Search from './components/Search';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import Header from "./components/Header";
+import Search from "./components/Search";
+
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
-  const [input, setInput] = useState('');
+  const [searchInput, setSearchInput] = useState("");
 
   const handleInput = (e) => {
     e.preventDefault();
-    console.log(input);
-  }
+    console.log(searchInput);
+    fetch(`https://api.unsplash.com/photos/random/?query=${searchInput}&client_id=${UNSPLASH_KEY}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  };
 
   return (
     <div>
       <Header title="Image Gallery" />
-      <Search input={input} onInput={setInput} onHandleInput={handleInput} />
+      <Search input={searchInput} onInput={setSearchInput} onHandleInput={handleInput} />
     </div>
   );
 }
