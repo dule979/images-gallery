8f26a90c8dc91e3b6c1011e7ee945accf075c140
Dusan Ilic <dule979@gmail.com>
2022-10-21 23:42:14 +0200

    Formating fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,20 +11,26 @@ function App() {
   const handleInput = (e) => {
     e.preventDefault();
     console.log(searchInput);
-    fetch(`https://api.unsplash.com/photos/random/?query=${searchInput}&client_id=${UNSPLASH_KEY}`)
-      .then(response => response.json())
-      .then(data => {
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${searchInput}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
         console.log(data);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-      })
+      });
   };
 
   return (
     <div>
       <Header title="Image Gallery" />
-      <Search input={searchInput} onInput={setSearchInput} onHandleInput={handleInput} />
+      <Search
+        input={searchInput}
+        onInput={setSearchInput}
+        onHandleInput={handleInput}
+      />
     </div>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 
 const Header = ({ title }) => {
-    return (
-        <Navbar bg="primary" variant="dark">
-        <Container>
-          <Navbar.Brand href="/">{title}</Navbar.Brand>
-        </Container>
-      </Navbar>
-    );
+  return (
+    <Navbar bg="primary" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
