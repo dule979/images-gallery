27ef88e83445571361055d151679f76b24ec6569
Dusan Ilic <dule979@gmail.com>
2022-10-23 02:31:59 +0200

    Created logo text component from svg file

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 
+import { Logo } from "../Icons";
+
 const Header = ({ title }) => {
   return (
-    <Navbar bg="primary" variant="dark">
+    <Navbar  variant="dark" style={{ backgroundColor: "#eee" }}>
       <Container>
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        <Logo style={{ maxWidth: "12rem", maxHeight: "2rem" }} />
       </Container>
     </Navbar>
   );
